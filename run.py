bc0657d670ac935d5b5029142472b85c27cda805
Jakob Brennan <jakob.brennan@gmail.com>
2023-04-08 21:01:19 +0000

    feat: code formatting

--- run.py
+++ run.py
@@ -26,10 +26,12 @@ ram_selected = 0
 gpu_selected = 0
 hdd_selected = 0
 
+
 def clear():
-    # check and make call for specific operating system 
+    # check and make call for specific operating system
     # (https://www.geeksforgeeks.org/clear-screen-python/)
     os.system('clear' if os.name == 'posix' else 'cls')
+
 
 def slow_print(msg):
     print(msg)
@@ -47,15 +49,27 @@ def main_menu():
         print(gpu_selected)
         print(hdd_selected)
         zeroprice = int(0)
-        partslist.update('E2', zeroprice) 
-        if cpu_selected >= 1 and gpu_selected >= 1 and ram_selected >= 1 and hdd_selected >= 1:
+        partslist.update('E2', zeroprice)
+        if cpu_selected >= 1 and gpu_selected >= 1 and ram_selected >= \
+                1 and hdd_selected >= 1:
             cpu_choice = partslist.acell('A2').value
             ram_choice = partslist.acell('B2').value
             gpu_choice = partslist.acell('C2').value
             hdd_choice = partslist.acell('D2').value
             total_price = partslist.acell('E7').value
             slow_print("Loading parts selection.... ")
-            print("CPU = " + cpu_choice + "\n" + "RAM = " + ram_choice + "\n" + "GPU = " + gpu_choice + "\n" + "HDD = " + hdd_choice)
+            print(
+                "CPU = " +
+                cpu_choice +
+                "\n" +
+                "RAM = " +
+                ram_choice +
+                "\n" +
+                "GPU = " +
+                gpu_choice +
+                "\n" +
+                "HDD = " +
+                hdd_choice)
             print("Calculating total price....")
             time.sleep(4)
             print("€" + total_price)
@@ -90,18 +104,27 @@ def main_menu():
 
                 def show_cpus():
                     for x in range(cpu_list):
-                        if cpus.cell((2+x), 1).value is not None:
+                        if cpus.cell((2 + x), 1).value is not None:
                             y = x + 1
                             y_string = str(y)
-                            entry_cpu = cpus.cell((2+x), 1)
-                            entry_core = cpus.cell((2+x), 2)
-                            entry_price = cpus.cell((2+x), 5)
+                            entry_cpu = cpus.cell((2 + x), 1)
+                            entry_core = cpus.cell((2 + x), 2)
+                            entry_price = cpus.cell((2 + x), 5)
                             print("\n")
-                            print(y_string + ". : " + entry_cpu.value + " " + entry_core.value + " Cores" + "\n Price: €" + entry_price.value)
-                            
-                        elif x+1 == range(cpu_list):
+                            print(
+                                y_string +
+                                ". : " +
+                                entry_cpu.value +
+                                " " +
+                                entry_core.value +
+                                " Cores" +
+                                "\n Price: €" +
+                                entry_price.value)
+
+                        elif x + 1 == range(cpu_list):
                             return
-                slow_print("Loading CPUs currently in stock, please be patient...")
+                slow_print(
+                    "Loading CPUs currently in stock, please be patient...")
                 show_cpus()
                 selected = input("Input the number of your chosen CPU.")
                 if selected == '1':
@@ -128,7 +151,7 @@ def main_menu():
                     partslist.update('A2', choice)
                     partslist.update('E3', cpu_price)
                     cpu_selected = cpu_selected + 1
-                    
+
                 else:
                     print("Not a valid selection choice. Exiting....")
                 clear()
@@ -142,17 +165,23 @@ def main_menu():
 
                 def show_ram():
                     for x in range(ram_list):
-                        if ram.cell((2+x), 1).value is not None:
+                        if ram.cell((2 + x), 1).value is not None:
                             y = x + 1
                             y_string = str(y)
-                            entry_name = ram.cell((2+x), 1)
-                            entry_price = ram.cell((2+x), 3)
+                            entry_name = ram.cell((2 + x), 1)
+                            entry_price = ram.cell((2 + x), 3)
                             print("\n")
-                            print(y_string + ". : " + entry_name.value + "\n Price: €" + entry_price.value)
-                            
-                        elif x+1 == range(ram_list):
+                            print(
+                                y_string +
+                                ". : " +
+                                entry_name.value +
+                                "\n Price: €" +
+                                entry_price.value)
+
+                        elif x + 1 == range(ram_list):
                             return
-                slow_print("Loading RAM currently in stock, please be patient...")
+                slow_print(
+                    "Loading RAM currently in stock, please be patient...")
                 show_ram()
                 selected = input("Input the number of your chosen RAM.")
                 if selected == '1':
@@ -194,18 +223,27 @@ def main_menu():
 
                 def show_gpus():
                     for x in range(gpus_list):
-                        if gpus.cell((2+x), 1).value is not None:
+                        if gpus.cell((2 + x), 1).value is not None:
                             y = x + 1
                             y_string = str(y)
-                            entry_name = gpus.cell((2+x), 1)
-                            entry_vram = gpus.cell((2+x), 2)
-                            entry_price = gpus.cell((2+x), 4)
+                            entry_name = gpus.cell((2 + x), 1)
+                            entry_vram = gpus.cell((2 + x), 2)
+                            entry_price = gpus.cell((2 + x), 4)
                             print("\n")
-                            print(y_string + ". : " + entry_name.value + " " + entry_vram.value + " " + "\n Price: €" + entry_price.value)
-                            
-                        elif x+1 == range(gpus_list):
+                            print(
+                                y_string +
+                                ". : " +
+                                entry_name.value +
+                                " " +
+                                entry_vram.value +
+                                " " +
+                                "\n Price: €" +
+                                entry_price.value)
+
+                        elif x + 1 == range(gpus_list):
                             return
-                slow_print("Loading GPUs currently in stock, please be patient...")
+                slow_print(
+                    "Loading GPUs currently in stock, please be patient...")
                 show_gpus()
                 selected = input("Input the number of your chosen GPU.")
                 if selected == '1':
@@ -253,18 +291,28 @@ def main_menu():
 
                 def show_hdd():
                     for x in range(hdd_list):
-                        if hdd.cell((2+x), 1).value is not None:
+                        if hdd.cell((2 + x), 1).value is not None:
                             y = x + 1
                             y_string = str(y)
-                            entry_name = hdd.cell((2+x), 1)
-                            entry_rpm = hdd.cell((2+x), 2)
-                            entry_price = hdd.cell((2+x), 4)
+                            entry_name = hdd.cell((2 + x), 1)
+                            entry_rpm = hdd.cell((2 + x), 2)
+                            entry_price = hdd.cell((2 + x), 4)
                             print("\n")
-                            print(y_string + ". : " + entry_name.value + " " + entry_rpm.value + "RPM " + "\n Price: €" + entry_price.value)
-                            
-                        elif x+1 == range(hdd_list):
+                            print(
+                                y_string +
+                                ". : " +
+                                entry_name.value +
+                                " " +
+                                entry_rpm.value +
+                                "RPM " +
+                                "\n Price: €" +
+                                entry_price.value)
+
+                        elif x + 1 == range(hdd_list):
                             return
-                slow_print("Loading storage devices currently in stock, please be patient...")
+                slow_print(
+                    "Loading storage devices currently in stock, \
+                                            please be patient...")
                 show_hdd()
                 selected = input("Input the number of your chosen HDD.")
                 if selected == '1':
@@ -302,8 +350,6 @@ def main_menu():
 
                 clear()
                 time.sleep(4)
-            
-
 
 
 def shop_intro():
@@ -314,5 +360,6 @@ def shop_intro():
     input("")
     slow_print("Proceeding...")
     main_menu()
+
 
 shop_intro()
